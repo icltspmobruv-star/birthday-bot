49a63cd9a3d0c93a4887e848bc155133f4070e66
The_Blacket <icltspmobruv@gmail.com>
2026-03-17 20:18:42 -0400

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,3 +1,13 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Bot is alive!");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running.");
+});
 const { Client, GatewayIntentBits, REST, Routes, SlashCommandBuilder, EmbedBuilder } = require("discord.js")
 const fs = require("fs")
 
